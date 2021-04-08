d7a2d8f890e7a16143f5649b2d8eba44d675d722
Xander Bakx <xanderbakx@gmail.com>
2021-04-08 16:53:16 -0400

    feat: login, logout, profile components created. https configured. reconfigured helmet for CSP

--- client/routes.jsx
+++ client/routes.jsx
@@ -2,15 +2,14 @@ import React from 'react';
 // import { connect } from 'react-redux'
 import { withRouter, Route, Switch } from 'react-router-dom';
 import {
-  Signup, Login, Logout, Home, Document,
+  Signup, Home, Document, Profile,
 } from './components';
 
 const Routes = () => (
   <Switch>
     <Route path="/document" component={Document} />
-    <Route path="/login" component={Login} />
-    <Route path="/logout" component={Logout} />
     <Route path="/signup" component={Signup} />
+    <Route path="/profile" component={Profile} />
     <Route path="/" component={Home} />
   </Switch>
 );
